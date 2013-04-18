1392d1cab8c43f08361ec127718d37d4aaf0a663
sebw <sebastien.wains@gmail.com>
2013-04-18 10:38:58 +0200

    getting around API limit to fetch all bookmarks (used to download the last 20 clips) display progress display statistics

---
+++ CHANGELOG.rst
@@ -0,0 +1,7 @@
+
+20130418
+--------
+- fix : getting around the limit of the API to fetch all clips
+- feature : display progress
+- feature : display statistics
+
--- kipptBackup.php
+++ kipptBackup.php
@@ -1,141 +1,204 @@
 <?php
-/**
- */
 
+/** deps : php5 php5-curl php5-mysql **/
 
 class kipptBackup {
-	var $userName;
-	var $apiToken;
+        var $userName;
+        var $apiToken;
 
-	var $dbName;
-	var $dbUser;
-	var $dbPass;
+        var $dbName;
+        var $dbUser;
+        var $dbPass;
 
-	public function setUsername($userName) {
-		$this->userName = $userName;
-	}
+        public function setUsername($userName) {
+                $this->userName = $userName;
+        }
 
-	public function setApiToken($apiToken) {
-		$this->apiToken = $apiToken;
-	}
+        public function setApiToken($apiToken) {
+                $this->apiToken = $apiToken;
+        }
 
-	public function setDatabaseCredentials($dbName, $dbUser, $dbPass) {
-		$this->dbName = $dbName;
-		$this->dbUser = $dbUser;
-		$this->dbPass = $dbPass;
-	}
+        public function setDatabaseCredentials($dbName, $dbUser, $dbPass) {
+                $this->dbName = $dbName;
+                $this->dbUser = $dbUser;
+                $this->dbPass = $dbPass;
+        }
 
-	public function createBackup() {
-		$this->connectToDatabase();
+        public function createBackup() {
+                $this->connectToDatabase();
 
-		echo 'Trying with kipp.com User "' . $this->userName . '" and API-Token "' . $this->apiToken . '"';
-		echo "\n\n";
+                /** get number of clips **/
+                $ch = curl_init('https://kippt.com/api/clips/?limit=1&offset=0');
 
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_HEADER, false);
+                curl_setopt(
+                        $ch,
+                        CURLOPT_HTTPHEADER,
+                        array(
+                                'X-Kippt-Username: ' . $this->userName,
+                                'X-Kippt-API-Token: ' . $this->apiToken
+                        )
+                );
 
-		$ch = curl_init('https://kippt.com/api/clips/');
+                $apiResult = curl_exec($ch);
+                curl_close($ch);
 
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt(
-			$ch,
-			CURLOPT_HTTPHEADER,
-			array(
-				'X-Kippt-Username: ' . $this->userName,
-				'X-Kippt-API-Token: ' . $this->apiToken
-			)
-		);
+                // decode the JSON data
+                $apiData = json_decode($apiResult);
 
-		$apiResult = curl_exec($ch);
-		curl_close($ch);
+                // See if the response contains some kind of a message - usually a hint that something
+                // went horribly wrong
+                if (isset ($apiData->message)) {
+                        echo 'Something went wrong:' . "\n";
+                        exit($apiData->message . "\n");
+                }
 
-		// decode the JSON data
-		$apiData = json_decode($apiResult);
+                // $count = number of clips
+                $count = $apiData->meta->total_count;
 
-		// See if the response contains some kind of a message - usually a hint that something
-		// went horribly wrong
-		if (isset ($apiData->message)) {
-			echo 'Something went wrong:' . "\n";
-			exit($apiData->message . "\n");
-		}
+		/** starting fetching clips **/
+                echo 'Backing up kippt.com ' . $count . ' bookmarks for user ' . $this->userName;
+                echo "\n";
 
-		if (isset($apiData->objects)) {
-			foreach ($apiData->objects as $clip) {
-				$this->compareOrImportClip($clip);
-			}
-		}
+		/** API limit is 200 **/
+                $limit = 100;
+                $offset = 0;
 
+		/** stats vars **/
+                global $stat_up2date;
+                global $stat_updated;
+                global $stat_new;
+                $stat_up2date = 0;
+                $stat_updated = 0;
+                $stat_new = 0;
+                
+		/** progress displayed in % **/        
+		$progresspc = round(($offset / $count * 100), 2);
 
-	}
+                // loop through clips
+                while ($offset <= $count) {
 
-	private function compareOrImportClip($clip) {
-		echo 'checking clip with ID=' . $clip->id;
+                        // display progress in %
+                        echo "Progress : " . $progresspc . "%\n";
 
-		$dbResult = mysql_query('SELECT id,updated FROM clips WHERE id=' . $clip->id);
-		if (mysql_num_rows($dbResult) == 1) {
-			echo "\n" . 'Clip exists already';
+                        $ch = curl_init('https://kippt.com/api/clips/?limit=' . $limit . '&offset=' . $offset);
 
-			$row = mysql_fetch_assoc($dbResult);
-			if ($row['updated'] < $clip->updated) {
-				echo "\n" . 'Clip exists, but seems to have changed, trying to update...';
-				$this->updateExistingClip($clip);
-			}
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                        curl_setopt($ch, CURLOPT_HEADER, false);
+                        curl_setopt(
+                                $ch,
+                                CURLOPT_HTTPHEADER,
+                                array(
+                                        'X-Kippt-Username: ' . $this->userName,
+                                        'X-Kippt-API-Token: ' . $this->apiToken
+                                )
+                        );
 
-		} else {
-			echo "\n" . 'New clip, inserting';
-			$this->insertNewClip($clip);
-		}
+                        $apiResult = curl_exec($ch);
+                        curl_close($ch);
 
-		echo "\n\n";
+                        // decode the JSON data
+                        $apiData = json_decode($apiResult);
 
-	}
+                        // See if the response contains some kind of a message - usually a hint that something
+                        // went horribly wrong
+                        if (isset ($apiData->message)) {
+                                echo 'Something went wrong:' . "\n";
+                                exit($apiData->message . "\n");
+                        }
 
-	private function updateExistingClip($clip) {
-		mysql_query('DELETE from clips WHERE id=' . $clip->id . ' LIMIT 1;');
+                        if (isset($apiData->objects)) {
+                                foreach ($apiData->objects as $clip) {
+                                        $this->compareOrImportClip($clip);
+                                }
+                        }
 
-		$this->insertNewClip($clip);
-	}
+                        $offset=$offset+$limit;
+                        if ($offset >= $count) {
+                                echo "Progress : 100%\n\n";
+				echo "Clips stats\n";
+				echo "-----------\n";
+                                echo "New ... : " . $stat_new . "\n";
+                                echo "Updated : " . $stat_updated . "\n";
+                                echo "Up2date : " . $stat_up2date . "\n\n";
+                        }
+                }
 
-	private function insertNewClip($clip) {
-		$importResult = mysql_query('INSERT into clips SET id="' . $clip->id .
-				'", title="' . $clip->title .'"' .
-				', url="' . $clip->url .'"' .
-				', list="' . $clip->list .'"' .
-				', notes="' . $clip->notes .'"' .
-				', url_domain="' . $clip->url_domain .'"' .
-				', created="' . $clip->created .'"' .
-				', updated="' . $clip->updated .'"' .
-				', resource_uri="' . $clip->resource_uri .'"'
-		);
+        }
 
-		if (!$importResult) {
-			echo 'INSERT error: ' . mysql_error() . "\n\n";
-		}
-	}
+        private function compareOrImportClip($clip) {
+                // echo 'Checking clip with ID=' . $clip->id ;
 
-	private function connectToDatabase() {
-		mysql_connect(
-			'localhost',
-			$this->dbUser,
-			$this->dbPass
-		);
-		mysql_select_db($this->dbName);
-		mysql_query('SET NAMES utf8;');
-	}
+                $dbResult = mysql_query('SELECT id,updated FROM clips WHERE id=' . $clip->id);
+                if (mysql_num_rows($dbResult) == 1) {
+                        // echo "\n" . 'Clip exists already... ';
+                        global $stat_up2date;
+                        $stat_up2date=$stat_up2date+1;
+
+                        $row = mysql_fetch_assoc($dbResult);
+                        if ($row['updated'] < $clip->updated) {
+                                global $stat_updated;
+                                $stat_updated=$stat_updated+1;
+                                echo "\n" . 'Updating clip ID ' . $clip->id;
+                                $this->updateExistingClip($clip);
+                        }
+
+                } else {
+                        echo "\n" . 'Inserting clip ID ' . $clip->id;
+                        global $stat_new;
+                        $stat_new=$stat_new+1;
+                        $this->insertNewClip($clip);
+                }
+
+        }
+
+        private function updateExistingClip($clip) {
+                mysql_query('DELETE from clips WHERE id=' . $clip->id . ' LIMIT 1;');
+                $this->insertNewClip($clip);
+        }
+
+        private function insertNewClip($clip) {
+                $importResult = mysql_query('INSERT into clips SET id="' . $clip->id .
+                                '", title="' . addslashes($clip->title) .'"' .
+                                ', url="' . $clip->url .'"' .
+                                ', list="' . $clip->list .'"' .
+                                ', notes="' . addslashes($clip->notes) .'"' .
+                                ', url_domain="' . $clip->url_domain .'"' .
+                                ', created="' . $clip->created .'"' .
+                                ', updated="' . $clip->updated .'"' .
+                                ', resource_uri="' . $clip->resource_uri .'"'
+                );
+
+                if (!$importResult) {
+                        echo 'INSERT error: ' . mysql_error() . "\n\n";
+                }
+        }
+
+        private function connectToDatabase() {
+                mysql_connect(
+                        'localhost',
+                        $this->dbUser,
+                        $this->dbPass
+                );
+                mysql_select_db($this->dbName);
+                mysql_query('SET NAMES utf8;');
+        }
+
 }
-
 
 $backup = new kipptBackup();
 
 // modify to suit your needs
-$backup->setUsername('johndoe');
-$backup->setApiToken('123456abcdef');
+$backup->setUsername('kippt_username');
+$backup->setApiToken('1234567890');
 $backup->setDatabaseCredentials(
-	'name of your local database',
-	'database username',
-	'database password'
+        'db_name',
+        'db_user',
+        'db_pwd'
 );
 
 $backup->createBackup();
 
-
 ?>
+
